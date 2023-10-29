932a27bb1f571fad00369becf9f5062a0d447a8a
Jacka10086 <111729221+Jacka10086@users.noreply.github.com>
2023-10-29 03:29:49 +0000

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,10 @@
+public class Cat extends Pet {
+
+    private String breed;
+
+    public Cat(String theName, int theAge, String theBreed) {
+        super(theName, theAge);
+        this.breed = theBreed;
+    }
+
+}
